3fe91a13eec4c7032298ca353273054a0e5dfbc0
cal-nyc <caalla3@gmail.com>
2023-10-01 23:36:02 -0400

    test update

--- pages/private/dashboard.tsx
+++ pages/private/dashboard.tsx
@@ -3,7 +3,7 @@ import Layout from "../../components/layout";
 export default function Dashboard() {
     return(
         <Layout title="Dashboard">
-            <h2>Dashboard</h2>
+            <h2>Dashboard!</h2>
         </Layout>
       );
 }
