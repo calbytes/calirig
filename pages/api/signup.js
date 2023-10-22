4bb849ddb0950533a89e4a82e6261d0c7e9ca7ea
calbytes <caalla3@gmail.com>
2023-10-22 14:16:23 -0400

    fixed signup / password check & cookies set

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -8,11 +8,13 @@ export default async function handler(req, res) {
   if (req.method == "POST"){
     const email = req.body['email'];
     const username = req.body['username'];
+    const password = req.body['password'];
     const cookies = new Cookies(req, res);
     cookies.set('role', "USER");
+    cookies.set('username', username);
 
-    const ipaddr = req.ipaddr;
-    logger.log("<SIGNUP> ipaddr: " + ipaddr + " | username: " + username 
+    const ipaddr = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    logger.log("<SIGNUP> ipaddr: " + ipaddr + " | username: " + username
       + " | email: " + email + " | pwd: " + password);
 
   }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,28 @@
 import Layout from '../components/layout';
+import React, { FormEvent } from 'react';
 
 export default function SignUp() {
+    async function checkPasswords(event: FormEvent<HTMLFormElement>){
+        const formData = new FormData(event.currentTarget);
+        var pwd = formData.get("password");
+        var pwdCheck = formData.get("passwordcheck");
+
+        if (pwd !== pwdCheck) {
+            alert("Passwords do not match");
+            event.preventDefault();
+            return false;
+        }
+        return true;
+    }
+
+
     return(
         <Layout title="Sign Up">
             <div className="mt-3 d-flex justify-content-center flex-column align-items-center">
                 <div>
                     <h3>Sign Up Form</h3>
                 </div>
-                <form className="border p-3" action='/api/signup' method='POST'>
+                <form className="border p-3" onSubmit={checkPasswords} action='/api/signup' method='POST'>
                     <div className="form-group">
                         <input name="username" id="username" type="text" placeholder='Username' required />
                     </div>
@@ -18,6 +33,10 @@ export default function SignUp() {
                     <br />
                     <div className="form-group">
                         <input name="password" id="password" type="password" placeholder='Password' required />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input name="passwordcheck" id="passwordcheck" type="password" placeholder='Re-Enter Password' required />
                     </div>
                     <br />
 
