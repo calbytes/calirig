081ef0ddf9814b5aff1752466da51b4bfad46737
calbytes <caalla3@gmail.com>
2023-11-23 15:25:41 -0500

    /api/contact forwards to db_server. added env variables

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,5 @@
 import log4js from 'log4js';
+import axios from '../../utils/axios'
 
 export default async function handler(req, res) {
   const logger = log4js.getLogger();
@@ -8,11 +9,22 @@ export default async function handler(req, res) {
     const name = req.body['name'];
     const email = req.body['email'];
     const msg = req.body['message'];
-    
     const ipaddr = req.headers['x-forwarded-for'] || req.socket.remoteAddress;    
-    logger.log("<CONTACT> ipaddr: " + ipaddr + " | name: " + name 
-      + " | email: " + email + " | msg: " + msg);
-  }
 
-  res.status(200).redirect("/");
+    const userData = {
+      name: name, 
+      email: email, 
+      message: msg, 
+      ip: ipaddr};
+    logger.log(userData);
+
+    try {
+      const response = await axios.post("/contact", userData);
+      //res.status(response.status).json(response.data);d
+      res.status(200).redirect("/contact");
+    } catch (error) {
+      logger.error('Error forwarding form data:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
 }
